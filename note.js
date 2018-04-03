4dc84796f8a7e0c1fd12671163287c3c8b56f923
Thomas Duffy <tomjamesduffy@gmail.com>
2018-04-03 17:57:55 +0100

    more tests

--- note.js
+++ note.js
@@ -4,3 +4,11 @@ function Note() {
   Note.prototype.writeNote = function(text) {
     this._text = text
   }
+
+  Note.prototype.display20 = function(note) {
+    return this._text.substring(0,20);
+  }
+
+  Note.prototype.display = function(note) {
+    return this._text;
+  }
