4041b28a5471d9f278726f7d79fe22f7df88b711
Charlene Bastians <charlenebastians@hotmail.com>
2018-04-04 11:41:04 +0100

    created global createNew method.

---
+++ scripts/createNewInstance.js
@@ -0,0 +1,9 @@
+"use strict";
+
+(function(exports) {
+function createNew(object) {
+  return new object;
+};
+
+  exports.createNew = createNew;
+})(this);
--- scripts/notepad.js
+++ scripts/notepad.js
@@ -3,10 +3,6 @@
     this._notes = []
 
   };
-    Notepad.prototype.createNote = function() {
-      var note = new Note()
-      return note
-    }
 
     Notepad.prototype.saveNote = function(note) {
       this._notes.push(note);
